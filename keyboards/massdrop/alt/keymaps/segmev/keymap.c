1fdc74c101bfb845e560c0ce455d06426540dfec
Stéphane K <steffkarraz@hotmail.com>
2020-02-24 23:14:07 +0100

    Space cadet added

--- keyboards/massdrop/alt/keymaps/segmev/keymap.c
+++ keyboards/massdrop/alt/keymaps/segmev/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         KC_CAPS, KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT, KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
+        KC_LSPO, KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSPC,          KC_UP,   KC_PGDN, \
         KC_LGUI, KC_LALT, KC_LCTL,                            KC_SPC,                             KC_RALT, MO(KEYBOARD_TUNE),KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [QWERTY_INT_TAP] = LAYOUT_65_ansi_blocker(
@@ -100,7 +100,9 @@ TG(QWERTY_INT_TAP), RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AU
 void matrix_init_user(void){};
 
 // Runs constantly in the background, in a loop.
-void matrix_scan_user(void){};
+void matrix_scan_user(void){
+        rgb_matrix_set_color(1, 0xff, 0xff, 0x00);
+};
 
 #define MODS_SHIFT (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
@@ -176,6 +178,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
+/* Post boot to disable underglow */
 void keyboard_post_init_user(void) {
     rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
     rgb_matrix_set_color_all(0, 0, 0);
